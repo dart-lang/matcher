8dc39dca6b8a2a197e29567f5300ffa162b1e1d2
Kevin Moore <kevmoo@google.com>
2020-05-01 11:13:29 -0700

    nit

--- test/map_matchers_test.dart
+++ test/map_matchers_test.dart
@@ -63,13 +63,6 @@ void main() {
     );
     shouldFail(
       {null: null},
-      containsPair('not null', null),
-      "Expected: contains pair 'not null' => <null> "
-      'Actual: {null: null} '
-      "Which:  doesn't contain key 'not null'",
-    );
-    shouldFail(
-      {null: null},
       containsPair(null, 'not null'),
       'Expected: contains pair <null> => \'not null\' '
       'Actual: {null: null} '
